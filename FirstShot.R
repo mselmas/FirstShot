433562a5a484e70e4ca513a8aba94a5b5f710978
mselmas <mselmas83@gmail.com>
2022-06-12 00:18:35 +0300

    Starter

---
+++ FirstShot.R
@@ -0,0 +1,12 @@
+print("Hello World")
+print("This is my first shot to delve into R galaxy")
+
+install.packages("esquisse")
+library()
+library(esquisse)
+
+library()
+sessionInfo()
+
+example <- matrix(c(12,34,56,78,90,98,76,65), nrow = 2, ncol = 4)
+
